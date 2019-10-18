e1ab9892ad78fc7cd851bbf81ca56c60d562eb87
Wejhe <56505566+wahyu-wejhe@users.noreply.github.com>
2019-10-18 21:10:37 +0700

    Front-End Updates
    - Theme changed : dark, blue, neon.
    - Scroll down button changed.
    - Mobile un-responsive fixed : disable mobile <meta> tag.
    - All-Texts and Pictures update.

---
+++ head.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>BlastOut 2020 | Feel The Real</title>
+<!-- Meta tag Keywords -->
+<!--<meta name="viewport" content="width=device-width, initial-scale=1">-->
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Blastout, blastout 2019, Tryout Klaten"/>
+<script>
+  window.addEventListener("load",function () {
+    var load_screen = document.getElementById('load-screen');
+    document.body.removeChild(load_screen);
+  });
+</script>
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!--// Meta tag Keywords -->
+<!-- css files -->
+<!-- online-fonts -->
+<link href="//fonts.googleapis.com/css?family=Ubuntu+Condensed&amp;subset=cyrillic,cyrillic-ext,greek,greek-ext,latin-ext" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Alegreya+Sans:100,100i,300,300i,400,400i,500,500i,700,700i,800,800i,900,900i&amp;subset=latin-ext,vietnamese" rel="stylesheet">
+<!-- //online-fonts -->
+<link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.css') ?>"> <!-- Bootstrap-Core-CSS -->
+<link rel="stylesheet" href="<?php echo  base_url('assets/css/style.css') ?>" type="text/css" media="all" /> <!-- Style-CSS -->
+<link href="<?php echo base_url('assets/css/stylee.css')?>" type="text/css" rel="stylesheet">
+<link rel="stylesheet" href="<?php echo base_url('assets/css/font-awesome.css')?>" > <!-- Font-Awesome-Icons-CSS -->
+<link rel="stylesheet" href="<?php echo base_url('assets/css/owl.carousel.css')?>" type="text/css" media="all">
+<link rel="stylesheet" href="<?php echo base_url('assets/css/chocolat.css')?>" type="text/css" media="screen">
+<!-- //css files -->
+<link rel="shortcut icon" href="<?php echo base_url(); ?>assets/ico/bo.jpg">
+</head>
